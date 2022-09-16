3de6ab2c5fa83ba2e66f012c0554ebabbb797f74
Daniel <daniel0ar@hotmail.com>
2022-09-15 22:05:59 -0500

    Modify index page

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,4 @@
 
-let t = 0; // variable de tiempo
 var canvas;
 
 function setup() {
@@ -7,28 +6,50 @@ function setup() {
   canvas.position(0,0);
   canvas.style('z-index','0');
   noStroke();
-  fill(0, 84, 247, 50);
+  frameRate(60);
+  fill(199,219,15);
+  // Establecer la posición de partida de la forma
+  xpos = width / 2;
+  ypos = height / 2;
 }
 
+
+let rad = 60; // El ancho de la forma
+let xpos, ypos; // Posición inicial de la forma
+
+let xspeed = 1.8; // La velocidad de la forma
+let yspeed = 1.5; // La velocidad de la forma
+
+let xdirection = 1; // Izquierda o derecha
+let ydirection = 1; // De arriba a abajo
+
 function draw() {
-  background(10, 10); // fondo translúcido (crea estelas)
+  background(0);
 
-  // grilla de elipses
-  for (let x = 0; x <= width; x = x + 100) {
-    for (let y = 0; y <= height; y = y + 100) {
-      // punto de partida de cada círculo depende de posición del ratón
-      const anguloX = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
-      const anguloY = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
-      // y también varía según la posición de la partícula
-      const angulo = anguloX * (x / width) + anguloY * (y / height);
+  // Actualizar la posición de la forma
+  xpos = xpos + xspeed * xdirection;
+  ypos = ypos + yspeed * ydirection;
 
-      // cada partícula se mueve en forma circular
-      const myX = x + 50 * cos(2 * PI * t + angulo);
-      const myY = y + 50 * sin(2 * PI * t + angulo);
-
-      ellipse(myX, myY, 15); // dibujar partícula
-    }
+  // Prueba para ver si la forma excede los límites de la pantalla
+  // Si lo hace, invierta su dirección multiplicando por -1
+  if (xpos > width - rad || xpos < rad) {
+    xdirection *= -1;
+  }
+  if (ypos > height - rad || ypos < rad) {
+    ydirection *= -1;
   }
 
-  t = t + 0.005 ; // actualizar tiempo
+  // Dibuja la forma
+  polygon(xpos, ypos, rad, 9);
+}
+
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
